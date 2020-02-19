8528a65ee78ea90e0791d4aefde01f64ae6bffc5
Edward Blanciak <blanciak.edward2@gmail.com>
2020-02-18 21:17:26 -0500

    app.js and PostsPage.js complete

--- src/App.js
+++ src/App.js
@@ -6,14 +6,18 @@ Look at each file to see where you need to pass props or add code
 */
 import React from "react";
 import "./App.css";
+import PostsPage from '../src/components/PostsContainer/PostsPage';
+import SearchBar from "../src/components/SearchBar/SearchBarContainer";
 // import the PostsPage and SearchBar and add them to the App
 
 
-const App = () => {
+const App = (props) => {
+console.log('app props: ',props); // giving me undefined, why?
 
   return (
     <div className="App">
-      {/* Add imported components here to render them */}
+      <PostsPage />
+      <SearchBar />
     </div>
   );
 };
